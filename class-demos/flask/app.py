02ab40eabe4617809c527710a4021499412b18f1
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2023-12-15 23:32:05 +0530

    migrations

--- class-demos/flask/app.py
+++ class-demos/flask/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/diksha'
 
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 class Person(db.Model):
     __tablename__ = 'persons'
